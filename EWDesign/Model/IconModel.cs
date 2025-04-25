b262042e8501c2e32f4d5310bcfb9e9912f63f24
Winston Connolly <lolgaker977@gmail.com>
2025-04-24 22:41:39 -0600

    New tab Items and Icon model

---
+++ EWDesign/Model/IconModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EWDesign.Model
+{
+    class IconModel
+    {
+        public string IconPath { get; set; }
+        public string Text { get; set; }
+        public string Tooltip { get; set; }
+        public ICommand Command { get; set; }
+    }
+}
--- EWDesign/ViewModel/NewViewModel.cs
+++ EWDesign/ViewModel/NewViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using EWDesign.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp1.Core;
 
 namespace EWDesign.ViewModel
 {
-    class NewViewModel
+    class NewViewModel:ObservableObject
     {
+        public ObservableCollection<IconModel> Icons { get; set; }
+
+        public NewViewModel()
+        {
+            Icons = new ObservableCollection<IconModel> {
+                new IconModel
+                {
+                    IconPath = "pack://application:,,,/Assets/newPageIcon.png",
+                    Text = "New",
+                    Tooltip = "New Page",
+                    Command = new RelayCommand(o => NewPage())
+                },
+                new IconModel
+                {
+                    IconPath = "pack://application:,,,/Assets/newPageIcon.png",
+                    Text = "Template 1",
+                    Tooltip = "New Template 1",
+                    Command = new RelayCommand(o => NewTemplate(1))
+                },
+                new IconModel
+                {
+                    IconPath = "pack://application:,,,/Assets/newPageIcon.png",
+                    Text = "Template 2",
+                    Tooltip = "New Template 2",
+                    Command = new RelayCommand(o => NewTemplate(2))
+                }
+            };
+        }
+
+        private void NewPage()
+        {
+
+        }
+
+        private void NewTemplate(int templateID)
+        {
+
+        }
     }
 }
